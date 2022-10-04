aab5e28cfb65d03554301806cf466c645594b187
Gradlax <valhama@protonmail.com>
2022-10-04 18:52:58 -0500

    theFirstandOnly

---
+++ script.js
@@ -0,0 +1,20 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Aaron is the coolest programmer on the block'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => 300 / e.target.value)
